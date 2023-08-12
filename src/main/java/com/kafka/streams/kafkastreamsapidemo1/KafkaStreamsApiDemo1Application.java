e30e518da08cf6f470392a2c3e1c5bc478a3d1a2
MoMutawe1 <mutalogix@gmail.com>
2023-08-12 10:07:33 -0500

    init commit

---
+++ src/main/java/com/kafka/streams/kafkastreamsapidemo1/KafkaStreamsApiDemo1Application.java
@@ -0,0 +1,13 @@
+package com.kafka.streams.kafkastreamsapidemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaStreamsApiDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaStreamsApiDemo1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/kafka/streams/kafkastreamsapidemo1/KafkaStreamsApiDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.streams.kafkastreamsapidemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStreamsApiDemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
